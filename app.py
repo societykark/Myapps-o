60d9a082589d0af9ab09ca401dfb2e98d1074b71
societykark <societykark@gmail.com>
2026-05-31 19:38:49 -0400

    Actualizar app.py

--- app.py
+++ app.py
@@ -926,4 +926,4 @@ def index():
     return render_template_string(HTML)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=False) 
